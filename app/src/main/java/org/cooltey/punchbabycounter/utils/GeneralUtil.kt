12bc6e9db28c96654d8c228917718efa92e2f9fb
cooltey <coolteygame@gmail.com>
2021-05-18 17:18:28 -0700

    Set correct today date

--- app/src/main/java/org/cooltey/punchbabycounter/utils/GeneralUtil.kt
+++ app/src/main/java/org/cooltey/punchbabycounter/utils/GeneralUtil.kt
@@ -1,14 +1,15 @@
 package org.cooltey.punchbabycounter.utils
 
-import android.util.Log
 import java.util.*
 
 object GeneralUtil {
 
     fun getToday(): Date {
         val calendar = Calendar.getInstance()
-        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH), 0, 0, 0)
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
         return calendar.time
     }
 
