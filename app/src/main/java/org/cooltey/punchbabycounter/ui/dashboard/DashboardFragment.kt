6d64d55bc4e218ca34982ad07bff46a0fd78345c
cooltey <coolteygame@gmail.com>
2021-05-19 17:47:39 -0700

    Dashboard update

--- app/src/main/java/org/cooltey/punchbabycounter/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/org/cooltey/punchbabycounter/ui/dashboard/DashboardFragment.kt
@@ -9,23 +9,24 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import org.cooltey.punchbabycounter.R
+import org.cooltey.punchbabycounter.database.Record
+import org.cooltey.punchbabycounter.databinding.FragmentDashboardBinding
+import org.cooltey.punchbabycounter.databinding.FragmentHomeBinding
+import org.cooltey.punchbabycounter.ui.home.HomeViewModel
+import org.cooltey.punchbabycounter.utils.Prefs
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    private lateinit var dashboardViewModel: DashboardViewModel
+    private var currentUserId = -1L
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        dashboardViewModel = DashboardViewModel(requireContext())
+        currentUserId = Prefs.getCurrentId(requireActivity())
+        return binding.root
     }
 }
--- app/src/main/java/org/cooltey/punchbabycounter/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/org/cooltey/punchbabycounter/ui/dashboard/DashboardViewModel.kt
@@ -1,13 +1,10 @@
 package org.cooltey.punchbabycounter.ui.dashboard
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class DashboardViewModel(context: Context) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
-    }
-    val text: LiveData<String> = _text
 }
