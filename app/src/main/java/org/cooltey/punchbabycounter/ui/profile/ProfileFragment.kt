58a06f24bd980f35e2a7349c3ac5d58bd0be298d
cooltey <coolteygame@gmail.com>
2021-06-05 16:29:55 -0700

    Set multiple profiles

--- app/src/main/java/org/cooltey/punchbabycounter/ui/profile/ProfileFragment.kt
+++ app/src/main/java/org/cooltey/punchbabycounter/ui/profile/ProfileFragment.kt
@@ -26,22 +26,20 @@ class ProfileFragment : Fragment() {
     private val calendar = Calendar.getInstance()
     private val dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
     private var currentUserId = -1L
+    private var userNumbers = 0
     private var enableVibration = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         profileViewModel = ProfileViewModel(requireContext())
-        currentUserId = Prefs.getCurrentId(requireActivity())
         enableVibration = Prefs.enableVibration(requireActivity())
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (currentUserId > 0) {
-           initObservable()
-        }
+        initObservable()
 
         binding.profileVibration.setOnCheckedChangeListener { _, b ->
             enableVibration = b
@@ -59,39 +57,54 @@ class ProfileFragment : Fragment() {
     }
 
     private fun initObservable() {
-        profileViewModel.getUserById(currentUserId).observe(viewLifecycleOwner, {
-            it?.let {
-                binding.profileFirstName.editText?.setText(it.firstName)
-                binding.profileLastName.editText?.setText(it.lastName)
-                binding.profileNickName.editText?.setText(it.nickName)
-                it.birthday?.let { date ->
-                    binding.profileBirthday.editText?.setText(dateFormat.format(date))
-                    calendar.timeInMillis = date.time
-                }
-                it.gender?.let { gender ->
-                    if (gender == "M") {
-                        binding.profileGenderMale.isChecked = true
-                    } else {
-                        binding.profileGenderFemale.isChecked = true
-                    }
-                }
-                binding.profileNote.editText?.setText(it.note)
-                binding.profileVibration.isChecked = enableVibration
-                currentUserId = it.uid
-            }
-        })
         profileViewModel.getUserList.observe(viewLifecycleOwner, { list ->
-            list.forEach {
+            userNumbers = list.size
+            list.forEach { user ->
                 val itemBinding = ViewProfileItemBinding.inflate(LayoutInflater.from(requireContext()))
-                itemBinding.profileLastName.text = it.lastName
-                itemBinding.profileFirstName.text = it.firstName
+                itemBinding.profileLastName.text = user.lastName
+                itemBinding.profileFirstName.text = user.firstName
+                user.birthday?.let { birthday ->
+                    itemBinding.profileBirthday.text = dateFormat.format(birthday)
+                }
+                itemBinding.profileEdit.setOnClickListener {
+                    profileViewModel.showList = false
+                    showViews()
+                    loadProfile(user.uid)
+                }
                 binding.profileList.addView(itemBinding.root)
             }
         })
     }
 
+    private fun loadProfile(userId: Long) {
+        profileViewModel.getUserById(userId).observe(viewLifecycleOwner, {
+            binding.profileFirstName.editText?.setText(it.firstName)
+            binding.profileLastName.editText?.setText(it.lastName)
+            binding.profileNickName.editText?.setText(it.nickName)
+            it.birthday?.let { date ->
+                binding.profileBirthday.editText?.setText(dateFormat.format(date))
+                calendar.timeInMillis = date.time
+            }
+            it.gender?.let { gender ->
+                if (gender == "M") {
+                    binding.profileGenderMale.isChecked = true
+                } else {
+                    binding.profileGenderFemale.isChecked = true
+                }
+            }
+            binding.profileNote.editText?.setText(it.note)
+            binding.profileVibration.isChecked = enableVibration
+            binding.profileActive.isChecked = it.uid == Prefs.getCurrentId(requireActivity())
+            if (userNumbers == 1) {
+                binding.profileActive.isEnabled = false
+            }
+            currentUserId = it.uid
+        })
+    }
+
     override fun onResume() {
         super.onResume()
+        profileViewModel.showList = true
         showViews()
     }
 
@@ -101,22 +114,27 @@ class ProfileFragment : Fragment() {
     }
 
     private fun showViews() {
-        if (currentUserId <= 0) {
-            binding.newProfileButton.visibility = View.VISIBLE
-            binding.scrollViewContainer.visibility = View.GONE
-            binding.profileListContainer.visibility = View.GONE
-            binding.newProfileButton.setOnClickListener {
+        when {
+            Prefs.getCurrentId(requireActivity()) <= 0 -> {
+                binding.newProfileButton.visibility = View.VISIBLE
+                binding.scrollViewContainer.visibility = View.GONE
+                binding.profileListContainer.visibility = View.GONE
+                binding.newProfileButton.setOnClickListener {
+                    binding.newProfileButton.visibility = View.GONE
+                    binding.scrollViewContainer.visibility = View.VISIBLE
+                }
+            }
+            profileViewModel.showList -> {
+                binding.profileList.removeAllViewsInLayout()
+                binding.newProfileButton.visibility = View.GONE
+                binding.scrollViewContainer.visibility = View.GONE
+                binding.profileListContainer.visibility = View.VISIBLE
+            }
+            else -> {
                 binding.newProfileButton.visibility = View.GONE
                 binding.scrollViewContainer.visibility = View.VISIBLE
+                binding.profileListContainer.visibility = View.GONE
             }
-        } else if (profileViewModel.showList){
-            binding.newProfileButton.visibility = View.GONE
-            binding.scrollViewContainer.visibility = View.GONE
-            binding.profileListContainer.visibility = View.VISIBLE
-        } else {
-            binding.newProfileButton.visibility = View.GONE
-            binding.scrollViewContainer.visibility = View.VISIBLE
-            binding.profileListContainer.visibility = View.GONE
         }
     }
 
@@ -155,10 +173,17 @@ class ProfileFragment : Fragment() {
             user.uid = currentUserId
         }
 
+        if (binding.profileActive.isChecked) {
+            Prefs.saveCurrentId(requireActivity(), currentUserId)
+        }
+
         profileViewModel.save(user) {
             Toast.makeText(requireContext(), R.string.toast_success, Toast.LENGTH_SHORT).show()
             Prefs.saveCurrentId(requireActivity(), it)
             Prefs.enableVibration(requireActivity(), enableVibration)
         }
+
+        profileViewModel.showList = true
+        showViews()
     }
 }
